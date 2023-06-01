9699386e5a026035e765244fa22ef5777724eeaf
GonzaloCastillo1483 <gonzalo.castillo1483@gmail.com>
2023-06-01 18:02:22 -0400

    navbar

--- ProyectoPrograWeb/resources/views/layouts/master.blade.php
+++ ProyectoPrograWeb/resources/views/layouts/master.blade.php
@@ -10,13 +10,29 @@
     <title>Inicio</title>
 </head>
 <body>
-    <div>
-        <div class="row">
-            <div class="col">
-                <button class="btn btn-primary">primary</button>
-            </div>
+    <!--Barra de busqueda-->
+    <nav class="navbar navbar-expand-lg  bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand text-white" href="#">TSI</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active text-white" aria-current="" href="#">Alumno</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white" href="#">Profesor</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white" href="#">Administrador</a>
+              </li>
+              
+            </ul>
+          </div>
         </div>
-    </div>
+      </nav>
 
     @yield('Inicio')
     
