3dbd10573ec35818af200c5547a0c26a7b5511d1
GonzaloCastillo1483 <gonzalo.castillo1483@gmail.com>
2023-06-02 23:30:55 -0400

    Vista estudiantes mejorada

--- ProyectoPrograWeb/resources/views/Estudiante/inicio.blade.php
+++ ProyectoPrograWeb/resources/views/Estudiante/inicio.blade.php
@@ -1,15 +1,16 @@
 @extends('layouts.master')
 
 @section('Inicio')
-<div class="container bg-light">
-    <h1 style="text-align: center">Propuesta Tsi</h1>
+<div class="container-fluid bg-light">
+    <h1 style="text-align: left" class="mt-2">Propuesta Tsi</h1>
     <div class="row">
-        <form action="">
-            <div class="card">
-                <div class="card-header">
-                    <h3 style="text-align: center">Propuesta</h3>
-                </div>
-                <div class="card-body">
+        <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6">
+            <div class="card mt-2">
+            <div class="card-header mt-2">
+                <h3 style="text-align: center">Ingresa tus datos</h3>
+            </div>
+            <div class="card-body">
+                <form action="">
                     <label for="nombre">Nombre</label>
                     <input type="text" id="nombre" name="nombre" class="form-control">
                     <label for="apellido">Apellido</label>
@@ -17,9 +18,62 @@
                     <label for="propuesta">Nombre de la propuesta</label>
                     <input type="text" id="propuesta" name="propuesta" class="form-control">
                     <h5 class="my-3">Sube el archivo en formato PDF</h5>
+                    <small>El archivo debe tener el nombre de la propuesta y de que trata</small>
+                    <div class="form-group">
+                        <label for="PDF">PDF</label>
+                        <input type="file" id="PDF" name="PDF" class="form-control">
+                        <button type="submit" class="btn btn-warning mt-2">Subir PDF</button>
+                    </div>   
+                </form>
+                
+            </div>
+          </div>
+        </div>
+        
+        <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6">
+            <div class="card">
+                <div class="card-header bg-light">
+                    <h3 style="text-align: center">Estado de propuestas</h3>
+                </div>
+                <div class="card-body">
+                    <table class="table table-striped table-hover">
+                        <thead>
+                          <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">Apellido</th>
+                            <th scope="col">Nombre Propuesta</th>
+                            <th scope="col">Estado de Propuesta</th>
+                          </tr>
+                        </thead>
+                        <tbody class="table-group-divider">
+                          <tr>
+                            <th scope="row">1</th>
+                            <td>Nombre 1</td>
+                            <td>Apellido</td>
+                            <td>Propuesta 1</td>
+                            <td>Estado 1</td>
+                          </tr>
+                          <tr>
+                            <th scope="row">2</th>
+                            <td>Nombre 2</td>
+                            <td>Apellido 2</td>
+                            <td>Propuesta 2</td>
+                            <td>Estado 2</td>
+                          </tr>
+                          <tr>
+                            <th scope="row">3</th>
+                            <td>Nombre 3</td>
+                            <td>Apellido 3</td>
+                            <td>Propuesta 3</td>
+                            <td>Estado 3</td>
+                          </tr>
+                        </tbody>
+                      </table>
                 </div>
             </div>
-        </form>
+        </div>
+       
         
 
     </div>
--- ProyectoPrograWeb/resources/views/layouts/master.blade.php
+++ ProyectoPrograWeb/resources/views/layouts/master.blade.php
@@ -9,7 +9,7 @@
 </head>
 <body>
     <!--Barra de busqueda-->
-    <nav class="navbar navbar-expand-lg  bg-dark">
+    <nav class="navbar navbar-expand-lg  bg-dark" style="height: 100px">
         <div class="container-fluid">
           <a class="navbar-brand text-white" href="#">TSI</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
